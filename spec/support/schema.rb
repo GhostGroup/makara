a14fffdc6c656debc812f42262c0a54d6d1c1e76
Kevin Choubacha <kchoubacha@weedmaps.com>
2016-06-15 16:57:14 -0700

    Clean up rubocop style suggestions

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -5,7 +5,7 @@ ActiveRecord::Schema.define(:version => 20130628161227) do
     t.string   "name"
   end
 
-  create_table :pictures, :force => true do |t|
+  create_table :pictures, force: true do |t|
     t.string  :name
     t.integer :imageable_id
     t.string  :imageable_type
