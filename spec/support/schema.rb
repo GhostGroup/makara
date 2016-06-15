fa0a31de86c30f1f291cb6f3e8c55f08d4c30a14
Kevin Choubacha <kchoubacha@weedmaps.com>
2016-06-15 15:56:51 -0700

    Add tests for polymorphic associations

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,9 +1,16 @@
 ActiveRecord::Schema.define(:version => 20130628161227) do
-
   drop_table "users" if table_exists? "users"
 
   create_table "users" do |t|
     t.string   "name"
   end
 
+  create_table :pictures, :force => true do |t|
+    t.string  :name
+    t.integer :imageable_id
+    t.string  :imageable_type
+    t.timestamps null: false
+  end
+
+  add_index :pictures, :imageable_id
 end
